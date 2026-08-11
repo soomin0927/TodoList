63ea23c1fc2999c48456612243f57d2fa54aa2bc
soomin0927 <ehfvls0927@naver.com>
2026-08-11 21:15:56 +0900

    #16 TodoInput 디자인 구현 - TodoInput 디자인 구현 - 이미지 / 아이콘 파일 추가

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="h-[60px] border-b border-gray-200">
-            <div className="mx-auto flex h-full w-full max-w-[1200px] px-4 py-3">
+        <header className="h-[60px] border-b border-gray-200"> 
+            <div className="mx-auto flex h-full w-full max-w-[1200px] py-3">
                 <Image
                     src="/images/logo.png"
                     alt="doit_logo"
@@ -14,3 +14,6 @@ export default function Header() {
         </header>
     );
 }
+
+
+
